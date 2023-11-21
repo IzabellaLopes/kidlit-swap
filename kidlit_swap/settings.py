9c19c31726be96f591f43a9cc1600ee6ac4dc318
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2023-11-21 18:57:34 +0000

    Update heroku allowed host

--- kidlit_swap/settings.py
+++ kidlit_swap/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kidlit-swap.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['kidlit-swap-96ca222a1195.herokuapp.com', 'localhost']
 
 
 # Application definition
