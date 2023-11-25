5390a1d2611a42317c19ff35aef47b9fb0475d61
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2023-11-25 23:29:24 +0000

    Add Book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+"""Models"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+class Book(models.Model):
+    """
+    Model for Book
+
+    """
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    added_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user", default=None
+        )
+    description = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    return_date = models.DateField(
+        auto_now=False, auto_now_add=False, null=True, blank=True
+    )
