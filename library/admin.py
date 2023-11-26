0118275261c8e3dcd05891a1487f1873e9296e84
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2023-11-26 12:09:08 +0000

    Update admin registration for Book model

--- library/admin.py
+++ library/admin.py
@@ -3,4 +3,9 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """Allows admin to manage books via the admin panel"""
+    list_filter = ('title', 'status')
+    list_display = ('title', 'slug', 'status')
+    search_fields = ('title', 'description')
