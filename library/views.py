cf0997ae550d075d30ee5e313eb9c91911ddf337
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2023-11-26 23:11:50 +0000

    Create BookList view

--- library/views.py
+++ library/views.py
@@ -1,8 +1,30 @@
 """Views"""
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Book
 
 
-def add(request):
-    return HttpResponse('Hello')
+class BookList(generic.ListView):
+    """
+    View to display a paginated list of books in alphabetical order
+    """
+    model = Book
+    template_name = 'books_list.html'
+    paginate_by = 8
+    
+    def get_queryset(self):
+        """
+        Retrieve the queryset of books, 
+        ordered alphabetically with available books first
+        """
+        return Book.objects.order_by('status', 'title')
+
+    def get_context_data(self, **kwargs):
+        """
+        Add additional context data for available and borrowed book counts
+        """
+        context = super().get_context_data(**kwargs)
+        context['available_count'] = Book.objects.filter(status='a').count()
+        context['borrowed_count'] = Book.objects.filter(status='b').count()
+        return context
