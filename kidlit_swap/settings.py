e3237a839e906b5ca5bfc6f0c58ebc5c6fb59a40
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2023-12-15 01:54:15 +0000

    Install and configure Whitenoise for static file serving on Heroku

--- kidlit_swap/settings.py
+++ kidlit_swap/settings.py
@@ -69,6 +69,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
